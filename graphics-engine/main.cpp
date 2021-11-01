59a85692239a3f48c03333a66bdb3a5a3710e59d
adamjsmith117 <adamjsmith117@gmail.com>
2021-10-31 18:53:41 -0600

    feat: window rendering

--- graphics-engine/main.cpp
+++ graphics-engine/main.cpp
@@ -2,13 +2,24 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+// Called by GLFW on window resize to dynamically update our glViewport
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+  glViewport(0, 0, width, height);
+}
+
 int main() {
+  // Init
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  // Initialize glfw window + set context
+  GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "graphics-engine", NULL, NULL);
   if (window == NULL) {
     std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
@@ -16,5 +27,25 @@ int main() {
   }
   glfwMakeContextCurrent(window);
 
+  // Validate that GLAD has initialized
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  // Set the OpenGL Viewport & register dynamic window resizing callback
+  glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  // Run the render loop
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  // Cleanup and terminate process
+  glfwTerminate();
   return 0;
 }
